38bb198a6a219743e1e129d8ecdca1a7a552b3d3
leepeace-sha <leepeace@gmail.com>
2026-02-23 18:21:56 +0000

    Add PDF.js library URLs to service worker

--- sw.js
+++ sw.js
@@ -8,6 +8,8 @@ const ASSETS = [
   "./app-icon.png",
   "./logo.png",
   "https://cdn.jsdelivr.net/npm/jspdf@2.5.1/dist/jspdf.umd.min.js"
+  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js",
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js"
 ];
 
 self.addEventListener("install", (event) => {
